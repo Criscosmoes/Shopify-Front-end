a75015dae5ffb9b99da634caa46a499e6fb41698
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2021-09-10 01:20:14 -0700

    fetched nasa pictures and loaded them on screen

--- src/components/PictureList/PictureList.js
+++ src/components/PictureList/PictureList.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./_picturelist.scss";
 import Card from "../Card/Card";
 
 const PictureList = () => {
-  const cardList = [1, 2, 3].map((cur) => {
-    return <Card />;
+  const [allPictures, setAllPictures] = useState([]);
+
+  const renderedPictures = allPictures.map((cur) => {
+    return (
+      <Card
+        key={cur.date}
+        date={cur.date}
+        author={cur.copyright}
+        description={cur.explanation}
+        image={cur.url}
+        title={cur.title}
+      />
+    );
   });
+
+  useEffect(() => {
+    const fetchPictures = async () => {
+      const res = await fetch(
+        "https://api.nasa.gov/planetary/apod?api_key=twjJbHAQRzxN7Tug2CDcROEh4JgBNPTQMkpRTlQ8&start_date=2020-01-03&end_date=2020-01-13"
+      );
+      const json = await res.json();
+      console.log(json);
+
+      setAllPictures(json);
+    };
+
+    fetchPictures();
+  }, []);
 
   return (
     <div className="list-container">
-      <ul className="card-container">{cardList}</ul>
+      <ul className="card-container">{renderedPictures}</ul>
     </div>
   );
 };
